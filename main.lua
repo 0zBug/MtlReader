ac272ffbd7fe0f3ac88a1958a121f9e6641075f4
Bug <77031032+0zBug@users.noreply.github.com>
2023-06-27 18:54:05 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,12 +3,12 @@ return function(File)
 	local Material
 	
 	for _, Line in next, string.split(readfile(File), "\n") do
-		if string.sub(string.gsub(Line, " ", ""), 1, 6) == "newmtl" then
-			Material = string.match(Line, "newmtl (.+)")
+		if string.sub(string.gsub(Line, "%s", ""), 1, 6) == "newmtl" then
+			Material = string.match(Line, "newmtl%s+(.+)")
 	
 			Materials[Material] = {}
-		elseif string.sub(string.gsub(Line, " ", ""), 1, 6) == "map_Kd" then
-			Materials[Material] = string.match(Line, "map_Kd (.+)\.png") .. ".png"
+		elseif string.sub(string.gsub(Line, "%s", ""), 1, 6) == "map_Kd" then
+			Materials[Material] = string.match(Line, "map_Kd%s+(.+)\.png") .. ".png"
 		end
 	end
 
